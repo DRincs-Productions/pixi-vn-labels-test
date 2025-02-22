46cf876b73dad4bab46e302829a53ef7b0c1a40f
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-22 10:52:28 +0000

    improve

--- src/characters.ts
+++ src/characters.ts
@@ -1,8 +1,10 @@
-export const sly = new Character("___pixivn_juliette___", {
+import { CharacterBaseModel, saveCharacter } from "@drincs/pixi-vn";
+
+export const juliette = new CharacterBaseModel("___pixivn_juliette___", {
     name: "Juliette",
     age: 25,
     icon: "https://firebasestorage.googleapis.com/v0/b/pixi-vn.appspot.com/o/public%2Fcharacters%2Fjuliette-square.webp?alt=media",
     color: "#ac0086",
 });
 
-saveCharacter([mc, james, steph, sly]);
+saveCharacter([juliette]);
